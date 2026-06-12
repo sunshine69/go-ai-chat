f36551e1077d4355e754529af67fd319fad21498
steve kieu <steve.kieu@sonichealthcare.com.au>
2026-06-12 14:40:26 +1000

    Dont mangle new line anymore lets keep it best source of truth

--- mcp-srv/tools-base.go
+++ mcp-srv/tools-base.go
@@ -699,10 +699,10 @@ func (t *BaseToolManager) replaceLinesInFile(_ context.Context, request mcp.Call
 		sb.WriteByte('\n')
 	}
 
-	replace = strings.TrimSpace(replace)
+	// replace = strings.TrimSpace(replace)
 	sb.WriteString(replace)
 	if len(tail) > 0 {
-		sb.WriteByte('\n')
+		// sb.WriteByte('\n')
 		sb.WriteString(strings.Join(tail, "\n"))
 	}
 	updated := sb.String()
